f2262a00158251ba96b306235848e9f0db3a282f
sihaya <sihaya@atreidis.nl.eu.org>
2018-10-07 22:33:19 +0200

    add routing

--- console-app/src/App.js
+++ console-app/src/App.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import {Container, Grid, Header, Menu} from "semantic-ui-react";
 import Transports from "./Transports";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 class App extends Component {
-    state = { activeItem: 'account' }
-
-    handleItemClick() {
-
-    }
-
     render() {
-        const { activeItem } = this.state;
-
         return (
-
+            <Router>
                 <Grid columns={2} container style={{ padding: '1em 0em' }}>
                     <Grid.Row>
                         <Grid.Column>
@@ -27,21 +20,22 @@ class App extends Component {
                             <Menu vertical secondary>
                                 <Menu.Item
                                     name='consignments'
-                                    active={activeItem === 'account'}
-                                    onClick={this.handleItemClick}
+                                    to={'/transports'}
+                                    as={Link}
                                 />
                                 <Menu.Item
                                     name='settings'
-                                    active={activeItem === 'settings'}
-                                    onClick={this.handleItemClick}
+                                    to={'/settings'}
+                                    as={Link}
                                 />
                             </Menu>
                         </Grid.Column>
                         <Grid.Column width={13}>
-                            <Transports/>
+                            <Route exact path="/transports" component={Transports} />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
+            </Router>
         );
     }
 }
