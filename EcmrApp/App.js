0a23c0d858a9dbc66d861b75e8bfac3d82d35fb4
sihaya <sihaya@atreidis.nl.eu.org>
2019-05-02 23:23:45 +0200

    show actual values

--- EcmrApp/App.js
+++ EcmrApp/App.js
@@ -22,46 +22,46 @@ class App extends Component<Props> {
         super(props);
 
         this.state = {
-            notes: []
+            contracts: [],
+            sortedContracts: []
         };
     }
 
-    renderItem(item) {
+    static Address = ({address}) => (
+        <View style={{flex: 1, flexDirection: 'row'}}>
+            <Icon name="location-arrow" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
+            <View style={{flex: 8}}>
+                <Text>{address.postalCode} {address.city} {address.country}</Text>
+                <Text>{address.address}</Text>
+                <Text>{address.name}</Text>
+            </View>
+        </View>
+    );
 
+    renderItem(item) {
+        const total = item.loads.reduce((acc, load) => acc + (load.quantity ? Number(load.quantity) : 0), 0);
 
         return (
-            <View style={{flex: 1, flexDirection: 'column'}}>
+            <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white', elevation: 5, marginBottom: 10}}>
                 <View style={{flex: 1, flexDirection: 'row', backgroundColor: 'powderblue', paddingTop: 10, paddingBottom: 10}}>
-                    <Text style={{flex: 2, fontWeight: 'bold', paddingRight: 5, paddingLeft: 5, paddingTop: 5, paddingBottom: 5}}>TRANSPORT 341133</Text>
+                    <Text style={{flex: 2, fontWeight: 'bold', paddingRight: 5, paddingLeft: 5, paddingTop: 5, paddingBottom: 5}}>TRANSPORT {item.id.substring(0, 8)}</Text>
                     <Text style={{flex: 1, textAlign: 'center', color: 'white', fontWeight: 'bold', right: 5, paddingRight: 5, paddingLeft: 5, paddingTop: 5, paddingBottom: 5, borderRadius: 15, backgroundColor: 'rgb(60, 167, 60)'}}>loading complete</Text>
                 </View>
-                <View style={{flex: 1, padding: 5}}>
-                    <Text style={{fontWeight: 'bold'}}>origin</Text>
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Icon name="location-arrow" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
-                        <View style={{flex: 8}}>
-                            <Text>1234 AA Vlaardingen</Text>
-                            <Text>Dirk de Derdelaan</Text>
-                            <Text>Bloemenzaak</Text>
-                        </View>
+                <View style={{flex: 1, padding: 5, paddingLeft: 10, paddingRight: 10, elevation: 1, marginBottom: 5, backgroundColor: 'white'}}>
+                    <Text style={{fontWeight: 'bold'}}>pickup</Text>
+                    <App.Address address={item.pickup}/>
+
+                    <View style={{flex: 1, flexDirection: 'row', paddingTop: 10, paddingBottom: 10}}>
+                        <Icon name="dropbox" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
+                        <Text style={{flex: 8}}>{total} packages</Text>
                     </View>
+                </View>
+                <View style={{flex: 1, padding: 5, paddingLeft: 10, paddingRight: 10}}>
+                    <Text style={{fontWeight: 'bold', paddingTop: 10}}>delivery</Text>
+                    <App.Address address={item.delivery}/>
                     <View style={{flex: 1, flexDirection: 'row', paddingTop: 10}}>
                         <Icon name="dropbox" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
-                        <Text style={{flex: 8}}>56 packages</Text>
-                    </View>
-                    <View style={{flex: 1, padding: 5, paddingTop: 20}}>
-                        <Text style={{fontWeight: 'bold'}}>destination</Text>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-                            <Icon name="location-arrow" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
-                            <View style={{flex: 8}}>
-                                <Text>1234 AA Vlaardingen</Text>
-                                <Text>Dirk de Derdelaan</Text>
-                            </View>
-                        </View>
-                    </View>
-                    <View style={{flex: 1, flexDirection: 'row', paddingTop: 10}}>
-                        <Icon name="dropbox" style={{flex: 1, color: 'rgb(0, 115, 209)'}} size={30} />
-                        <Text style={{flex: 8}}>56 packages</Text>
+                        <Text style={{flex: 8}}>{total} packages</Text>
                     </View>
                 </View>
             </View>
@@ -72,12 +72,9 @@ class App extends Component<Props> {
         return (
             <View style={styles.container}>
                 <SectionList
-                    sections={[
-                        {title: '12-01-2018', data: ['Devin', 'Devin']},
-                        {title: '24-01-2018', data: ['Jackson']},
-                    ]}
+                    sections={this.state.sortedContracts}
                     renderItem={({item}) => this.renderItem(item)}
-                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title ? section.title : "unknown date"}</Text>}
                     keyExtractor={(item, index) => index}
                 />
             </View>
@@ -86,10 +83,24 @@ class App extends Component<Props> {
 
     async componentDidMount() {
         const response = await API.graphql(graphqlOperation(queries.listContracts));
-        const contracts = response.data.listContracts.items.sort((a, b) => a.updatedAt < b.updatedAt ? 1 : -1);
+        const contracts = response.data.listContracts.items.sort((a, b) => a.arrivalDate < b.arrivalDate ? 1 : -1);
+        const groupedContracts = contracts.reduce((acc, contract) => {
+            const arrivalDate = contract.arrivalDate;
+            if (acc[arrivalDate]) {
+                acc[arrivalDate].data.push(contract);
+            } else {
+                acc[arrivalDate] = {
+                    title: arrivalDate,
+                    data: [contract]
+                };
+            }
+            return acc;
+        }, {});
+        const sortedContracts = Array.from(Object.values(groupedContracts));
 
         this.setState({
-            notes: contracts
+            contracts: contracts,
+            sortedContracts: sortedContracts
         });
     }
 }
@@ -99,7 +110,6 @@ export default withAuthenticator(App, true);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 22
     },
     sectionHeader: {
         paddingTop: 5,
@@ -109,6 +119,14 @@ const styles = StyleSheet.create({
         fontSize: 14,
         fontWeight: 'bold',
         backgroundColor: 'rgba(247,247,247,1.0)',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+        elevation: 2
     },
     item: {
         padding: 10,
