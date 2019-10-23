de80a857eecebfe8aa1302b542c7e13576db3526
sihaya <sihaya@atreidis.nl.eu.org>
2019-10-23 21:56:49 +0200

    add activity information to transport screen

--- console-app/src/Transport.js
+++ console-app/src/Transport.js
@@ -7,12 +7,33 @@ import {
     Header,
     Icon,
     Step,
-    List
+    List, Table, Label, Divider, Segment
 } from "semantic-ui-react";
 import { API, graphqlOperation } from 'aws-amplify';
 import moment from 'moment';
 import * as queries from "./graphql/queries";
 import * as mutations from "./graphql/mutations";
+
+const Address = ({address, label, icon}) => (
+    <Container>
+        { label &&
+            <Header as={'h5'}>
+            <Icon name={icon} />
+            <Header.Content>{label}</Header.Content>
+            </Header>
+        }
+        <List>
+            <List.Item>
+                <List.Content><strong>{address.name}</strong></List.Content>
+            </List.Item>
+            <List.Item>
+                <List.Content>{address.address}</List.Content>
+            </List.Item>
+            <List.Item>
+                <List.Content>{address.postalCode} {address.city}, {address.country}</List.Content>
+            </List.Item>
+        </List>
+    </Container>);
 
 class Transport extends Component {
     constructor(props) {
@@ -34,40 +55,19 @@ class Transport extends Component {
         });
     }
 
-    renderAddress(address, label, icon) {
-        return (
-            <Container>
-                <Header as={'h5'}>
-                    <Icon name={icon} />
-                    <Header.Content>{label}</Header.Content>
-                </Header>
-                <List>
-                    <List.Item>
-                        <List.Content><strong>{address.name}</strong></List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Content>{address.address}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Content>{address.postalCode}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Content>{address.city}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Content>{address.country}</List.Content>
-                    </List.Item>
-                </List>
-            </Container>
-        )
-    }
-
     render() {
         const {contract} = this.state;
 
         if (!contract) {
             return (<div>loading</div>);
         }
+
+        const goods =
+            <List>
+                {contract.loads.map((load) =>
+                    <List.Item>{load.quantity} {load.category}, {load.description}</List.Item>
+                )}
+            </List>;
 
         return (
             <Container>
@@ -86,31 +86,72 @@ class Transport extends Component {
                     <Header.Subheader>Created by {contract.owner} on {moment(contract.createdAt).format("LLLL")}</Header.Subheader>
                 </Header>
 
-
-
+                <Step.Group>
+                    <Step content='Created' active/>
+                    <Step content='Ongoing' />
+                    <Step content='Done' />
+                    <Step content='Archived' />
+                </Step.Group>
+                <Segment>
                 <Grid columns={3}>
-                    <Grid.Row columns={1}>
+                    <Grid.Row>
                         <Grid.Column>
-                            <Step.Group>
-                                <Step content='Created' active/>
-                                <Step content='Ongoing' />
-                                <Step content='Done' />
-                                <Step content='Archived' />
-                            </Step.Group>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row divided>
-                        <Grid.Column>
-                            {this.renderAddress(contract.shipper, 'Shipper', 'building')}
+                            <Address address={contract.shipper} icon={'building'} label={'Shipper'}/>
                         </Grid.Column>
                         <Grid.Column>
-                            {this.renderAddress(contract.carrier, 'Carrier', 'truck')}
+                            <Address address={contract.carrier} icon={'truck'} label={'Carrier'}/>
                         </Grid.Column>
                         <Grid.Column>
                             Transport code
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
+                </Segment>
+                <Segment>
+                    <Grid columns={5} divided>
+                        <Grid.Row divided>
+                            <Grid.Column><Header as={'h5'}>Activity</Header></Grid.Column>
+                            <Grid.Column><Header as={'h5'}>Address</Header></Grid.Column>
+                            <Grid.Column><Header as={'h5'}>Loads</Header></Grid.Column>
+                            <Grid.Column><Header as={'h5'}>Arrival</Header></Grid.Column>
+                            <Grid.Column><Header as={'h5'}>Driver</Header></Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Label circular={true}>1</Label>Pickup
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Address address={contract.pickup} />
+                            </Grid.Column>
+                            <Grid.Column>
+                                {goods}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {contract.arrivalDate}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {contract.carrierUsername}
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Label circular={true}>2</Label>Delivery
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Address address={contract.delivery} />
+                            </Grid.Column>
+                            <Grid.Column>
+                                {goods}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {contract.deliveryDate}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {contract.carrierUsername}
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
             </Container>
 
         );
